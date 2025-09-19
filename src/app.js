cd0be7339e8c2117f45428d3f7f10eb379504725
Kavya Gupta <kavyagupta0605@gmail.com>
2025-09-19 20:10:44 +0530

    utils code discussion

--- src/app.js
+++ src/app.js
@@ -3,4 +3,18 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+//Implementing security practices to handle the various types and forms of requests incoming from frontend
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); //extended means object ke andar object use kar skte ho
+app.use(express.static("public"));
+app.use(cookieParser());
+
 export { app };
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = () => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+//Below is the try catch way of handling the situation
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
